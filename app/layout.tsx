6a7f07c9f7c9a4e380b163cd61af414438444454
JosueRhea <josueroyale8171@gmail.com>
2023-06-15 16:20:31 -0600

    Add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Nav } from "@/components/nav";
 import { Header } from "@/components/header";
+import { Login } from "@/components/login";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,18 +13,26 @@ export const metadata = {
   description: "Track your habits and goals.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const shouldShowLogin = session == null;
+
   return (
     <html lang="en">
       <body className={`${inter.className} flex flex-col items-center px-4`}>
         <main className="w-full max-w-4xl">
           <Header />
           <Nav />
-          {children}
+          {shouldShowLogin ? <Login /> : children}
         </main>
       </body>
     </html>
---
+++ components/login.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { GoogleIcon } from "@/icons/google";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export function Login() {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: typeof window !== "undefined" ? window.location.origin : "",
+      },
+    });
+  };
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_, session) => {
+      router.refresh();
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <div className="w-full flex justify-center mt-24">
+      <Card className="w-full max-w-xs">
+        <CardHeader>
+          <CardTitle className="text-center">Sign in</CardTitle>
+          <CardDescription className="text-center">
+            Enter with your google account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Button onClick={handleLogin}>
+            <GoogleIcon className="h-4 w-4 mr-2" /> Go!
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
