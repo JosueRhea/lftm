d7e25588daa1b10e6dbf256e332f57214ebd09ba
JosueRhea <josueroyale8171@gmail.com>
2023-06-19 15:45:35 -0600

    Fix: extra skeleton

--- components/activities.tsx
+++ components/activities.tsx
@@ -35,7 +35,6 @@ export function Activities({ activities, userId }: Props) {
   if (isLoading && !data) {
     return (
       <div className="mt-4">
-        <Skeleton className="h-8 w-44 rounded-md" />
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 mt-4 gap-4">
           {[...Array(8)].map((_, i) => (
             <Skeleton key={i} className="h-10 w-full rounded-md" />
