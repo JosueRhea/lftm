b83506eeda90f0414c702559a47ad9af7caf0b66
Josue A <73492768+JosueRhea@users.noreply.github.com>
2023-06-20 15:20:42 +0000

    Chore: add underlined nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,25 +3,31 @@ import { Activity, Home } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
+import { motion } from "framer-motion";
 
 type LinkItemProps = {
   href: string;
   children: ReactNode;
 };
 
+const links = [
+  {
+    href: "/",
+    name: "Home",
+    icon: Home,
+  },
+  {
+    href: "/report",
+    name: "Report",
+    icon: Activity,
+  },
+];
+
 function LinkItem({ href, children }: LinkItemProps) {
-  const pathname = usePathname();
-
-  const isActive = pathname === href;
-
   return (
     <Link
       href={href}
-      className={`text-primary text-center w-full border-b-2 ${
-        isActive
-          ? "border-primary text-primary"
-          : "border-transparent bg-transparent text-accent-foreground"
-      } h-8 flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background`}
+      className={`text-primary relative text-center w-full h-10 flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background`}
     >
       {children}
     </Link>
@@ -29,14 +35,25 @@ function LinkItem({ href, children }: LinkItemProps) {
 }
 
 export function Nav() {
+  const pathname = usePathname();
   return (
     <nav className="w-full grid grid-cols-2 mt-4">
-      <LinkItem href="/">
-        <Home className="w-4 h-4 mr-2" /> Home
-      </LinkItem>
-      <LinkItem href="/report">
-        <Activity className="w-4 h-4 mr-2" /> Report
-      </LinkItem>
+      {links.map(({ href, name, icon: Icon }) => {
+        const isActive = pathname === href;
+
+        return (
+          <LinkItem key={href} href={href}>
+            <Icon className="w-4 h-4 mr-2" />
+            {name}
+            {isActive && (
+              <motion.div
+                className="absolute bottom-0 right-0 left-0 h-[2px] bg-primary"
+                layoutId="underline"
+              ></motion.div>
+            )}
+          </LinkItem>
+        );
+      })}
     </nav>
   );
 }
